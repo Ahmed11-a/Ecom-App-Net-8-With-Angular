628977d2e33e3c09246e020bf8f2448236fd20ac
Eng./Ahmed Mahmoud <167317807+A212Commatecs@users.noreply.github.com>
2025-04-01 21:53:35 +0200

    Add project files.

---
+++ Ecom.Infrastructure/Repository/PhotoRepositry.cs
@@ -0,0 +1,18 @@
+﻿using Ecom.Core.Entities.Product;
+using Ecom.Core.Interfaces;
+using Ecom.Infrastructure.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecom.Infrastructure.Repository
+{
+    public class PhotoRepositry : GenericRepository<Photo>, IPhotoRepository 
+    {
+        public PhotoRepositry(AppDbContext context) : base(context)
+        {
+        }
+    }
+}
---
+++ Ecom.Infrastructure/infrastructureRegisteration.cs
@@ -0,0 +1,30 @@
+﻿using Ecom.Core.Interfaces;
+using Ecom.Infrastructure.Data;
+using Ecom.Infrastructure.Repository;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecom.Infrastructure
+{
+    public static class infrastructureRegisteration
+    {
+        public static IServiceCollection infrastructureConfiguration(this IServiceCollection services,IConfiguration configration)
+        {
+            services.AddScoped(typeof(IGenericRepositry<>), typeof(GenericRepository<>));
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
+            services.AddDbContext<AppDbContext>(options =>
+            {
+                options.UseSqlServer(configration.GetConnectionString("EcomDatabase"));
+            });
+           
+             
+            return services;
+        }
+    }
+}
